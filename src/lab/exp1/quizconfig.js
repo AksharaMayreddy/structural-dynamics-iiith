64f24bb2379294a30f9b74ca18433e5ffd73ffc5
AksharaMayreddy <44540262+AksharaMayreddy@users.noreply.github.com>
2018-11-08 13:10:14 +0530

    Update quizconfig.js

--- src/lab/exp1/quizconfig.js
+++ src/lab/exp1/quizconfig.js
@@ -24,14 +24,20 @@ for (q=1;q<=totalquestions;q++){
 	var thequestion=eval("document.myquiz.question"+q)
 	for (c=0;c<thequestion.length;c++){
 		if (thequestion[c].checked==true)
-		actualchoices[q]=thequestion[c].value
+		{
+			actualchoices[q]=thequestion[c].value
+		}
 		}
 		
 	if (actualchoices[q]!=correctchoices[q]){ //process an incorrect choice
 		if (incorrect==null)
-		incorrect=q
+		{
+			incorrect=q
+		}
 		else
+		{
 		incorrect+="/"+q
+		}
 		}
 	}
 
